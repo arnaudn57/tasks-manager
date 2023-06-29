85982dfbe0c94b5d73a73c54dc53510ec565cb37
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2023-06-29 20:05:39 +0200

    Customers Controller && Views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,34 @@
 class CustomersController < ApplicationController
+
+
+    def index
+        @customers = Customer.all
+    end
+
+    def show
+        @customer = Customer.find(params[:id])
+        @company = Company.find(params[:company_id])
+    end
+
+    def new
+        @customer = Customer.new
+        @company = Company.find(params[:company_id])
+    end
+
+    def create
+        @customer = Customer.new(customer_params)
+        @customer.company_id = params[:company_id]
+        @company = Company.find(params[:company_id])
+        if @customer.save
+            redirect_to company_customers_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def customer_params
+        params.require(:customer).permit(:name, :description, :company_id)
+    end
 end
